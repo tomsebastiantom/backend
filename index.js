89fe19aa9124ce2c76a8533b2dac1504b85fa454
Tom Sebastian <tomsebastiantom@gmail.com>
2022-03-01 15:55:07 -0800

    Port edited

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.use("/search", searchs);
 
 connectDB();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
