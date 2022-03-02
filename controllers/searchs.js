a47ff98427b63e6087b848b5d09ccda461a090cd
Tom Sebastian <tomsebastiantom@gmail.com>
2022-03-01 16:08:30 -0800

    Callbacks Fixed

--- controllers/searchs.js
+++ controllers/searchs.js
@@ -4,36 +4,35 @@ const { Question } = require("../models/questions");
 
 exports.getQuestions = async (req, res, next) => {
   try {
-    const topics = await Topic.find({
+
+    
+    await Topic.find({
       $or: [
         { topic: req.query.q },
         { path: new RegExp(`,${req.query.q},`, "i") },
       ],
-    }).select("_id");
-
-    if (!topics) {
-      return next(new ErrorResponse("No topics found", 404));
-    }
-    topics
-      .then(async(topics) => {
-
-
+    })
+      .select("_id")
+      .then(async (topics) => {
         const questions = await Question.find({
           annotations: { $in: topics },
-        }).select("questionno -_id");
+        })
+          .select("questionno -_id")
+          .then((questions) => {
+            res.status(200).json(questions);
+          });
       })
-
-
       .catch((err) => {
-
-        if (!questions) {
-          return next(new ErrorResponse("No topics found", 404));
-        }
+        // if (!questions) {
+        //   return next(new ErrorResponse("No topics found", 404));
+        // }
         return next(new ErrorResponse(err, 500));
       });
 
-
-    res.status(200).json(questions);
+    // // if (!topics) {
+    // //   return next(new ErrorResponse("No topics found", 404));
+    // // }
+    // topics
   } catch (err) {
     next(new ErrorResponse(`Search couldnot due to server error ${err}`, 500));
   }
